50c4678bcd53e54b5107eb05f68cd92205b9ab7d
ahmadmunim <ahmadmunim0803@gmail.com>
2023-10-08 15:30:30 -0400

    finished frontend

--- static/js/preload-images.js
+++ static/js/preload-images.js
@@ -1,9 +1,36 @@
+console.log('hello')
 var images = [
-    "images/580b585b2edbce24c47b270f.png",
-    "images/mercury (1).png",
-    "images/venus.png.opdownload"
+    "../static/img/580b585b2edbce24c47b270f.png",
+    "../static/img/mercury (1).png",
+    "../static/img/venus.png"
 ];
+
+// Preload the images
 images.forEach(function (imgSrc) {
     var img = new Image();
     img.src = imgSrc;
+});
+
+// Function to scroll to a specific section
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    console.log(section)
+    if (section) {
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
+
+// Add click functionality to the images
+document.addEventListener("DOMContentLoaded", function () {
+    var imageElements = document.querySelectorAll(".planet-image");
+
+    imageElements.forEach(function (imageElement, index) {
+        imageElement.addEventListener("click", function () {
+            // Scroll to the corresponding section when the image is clicked
+            scrollToSection("section" + index); // Assumes your sections have IDs like "section0", "section1", etc.
+        });
+    });
 });

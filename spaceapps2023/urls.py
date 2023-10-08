300547567566de078d7171644e5192a01b6828e9
ahmadmunim <ahmadmunim0803@gmail.com>
2023-10-08 19:36:51 -0400

    trying to join streamlit and django

--- spaceapps2023/urls.py
+++ spaceapps2023/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from spaceapps2023.views import home
 from spaceapps2023.views import about_page
 from spaceapps2023.views import map_page
+from spaceapps2023.views import streamlit_integration
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('index.html', home, name='home'),
     path('about.html', about_page, name='about'),
     path('map.html', map_page, name = 'map'),
+    path('streamlit_integration.html', streamlit_integration, name='app4a')
 ]
--- spaceapps2023/views.py
+++ spaceapps2023/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import psutil
+import subprocess
 
 def home(req):
     return render(req, 'index.html')
@@ -8,3 +11,20 @@ def about_page(req):
 
 def map_page(req):
     return render(req, 'map.html')
+
+def streamlit_integration(req):
+    proc = subprocess.Popen(["streamlit", "run", "app4a.py"])
+    proc.wait()
+
+# Get the process details using psutil
+    process = psutil.Process(proc.pid)
+
+    # Get the list of network connections for the process
+    connections = process.connections(kind="inet")
+
+    # Extract the IP address from the connections
+    ip_address = connections[0].laddr.ip  # Assuming the first connection contains the IP address
+
+    print("IP Address:", ip_address)
+
+    return HttpResponse("Streamlit app is running on a separate server.")
